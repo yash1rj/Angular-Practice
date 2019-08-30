1676d872a0ae56541261391490937aff0aa3b795
Yash Raj <yashrj130@gmail.com>
2019-08-30 17:23:10 +0530

    Create contact.component.ts

---
+++ Routing/contact/contact.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-contact',
+  template: `<h1>Contact Me</h1>
+              <p> https://github.com/yash1rj </p>`
+})
+export class ContactComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
