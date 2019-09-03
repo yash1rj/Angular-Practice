51ea9083db83a1e94c3a019e2d136d3749c9294f
Yash Raj <yashrj130@gmail.com>
2019-09-03 13:24:43 +0530

    child-routes-nested-routes

--- Routing/app.routes.ts
+++ Routing/app.routes.ts
@@ -5,13 +5,24 @@ import { ContactComponent } from './contact/contact.component'
 import { ProductComponent } from './product/product.component'
 import { ErrorComponent } from './error/error.component'
 
-import { ProductDetailsComponent } from './product/product-details/product-details.component'
+import { ProductDetailsComponent } from './product/product-details/product-details.component';
+import { ProductOverviewComponent} from './product/product-details/product-overview.component';
+import { ProductSpecComponent} from './product/product-details/product-spec.component';
 
 export const appRoutes: Routes = [
   { path: 'home', component: HomeComponent },
   { path: 'contact', component: ContactComponent },
-  { path: 'product', component: ProductComponent },
-  { path: 'product/:id', component: ProductDetailsComponent },
+  { path: 'product', component: ProductComponent, 
+    children: [
+        { path: 'detail/:id', component: ProductDetailsComponent, 
+            children : [
+                { path: 'overview', component: ProductOverviewComponent },
+                { path: 'spec', component: ProductSpecComponent },  
+                { path: '', redirectTo:'overview', pathMatch:"full" }
+            ]
+        }
+      ]
+  },
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: '**', component: ErrorComponent }
 ];
