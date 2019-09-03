cc6fbaf1e880acdc1240be05844111fb17129607
Yash Raj <yashrj130@gmail.com>
2019-09-03 13:46:21 +0530

    Update product-details.component.ts

--- Routing/product/product-details/product-details.component.ts
+++ Routing/product/product-details/product-details.component.ts
@@ -12,53 +12,38 @@ export class ProductDetailsComponent implements OnInit {
 
   product:Product;
   id;
-  
+  sub;
+
   constructor(private _Activatedroute:ActivatedRoute,
               private _router:Router,
               private _productService:ProductService){
   }
 
-  /* Using snapshot */
-  // ngOnInit() {
-
-  //    //This still works but is deprecated
-  //    //this.id=this._Activatedroute.snapshot.params['id'];  
-
-  //    this.id=this._Activatedroute.snapshot.paramMap.get("id");
-
-    
-  //    let products=this._productService.getProducts();
-  //    this.product=products.find(p => p.productID==this.id);
-  // }
-
-  /* Using Subscribe */
-   
-  sub;
 
   ngOnInit() {
 
-    this.sub=this._Activatedroute.paramMap.subscribe(params => { 
-        console.log(params);
-        this.id = params.get('id'); 
-        let products=this._productService.getProducts();
-        this.product=products.find(p => p.productID==this.id);    
-    });
+      this.sub=this._Activatedroute.params.subscribe(params => { 
+          this.id = params['id']; 
+          let products=this._productService.getProducts();
+          this.product=products.find(p => p.productID==this.id);    
+      
+      });
+   }
 
-    // This params is deprecated
-
-    //this.sub=this._Activatedroute.params.subscribe(params => { 
-    //    this.id = params['id']; 
-    //    let products=this._productService.getProducts();
-    //    this.product=products.find(p => p.productID==this.id);    
-    //
-    //});
+  /* Using snapshot
+  ngOnInit() {
+      this.id=this._Activatedroute.snapshot.params['id'];
+      let products=this._productService.getProducts();
+      this.product=products.find(p => p.productID==this.id);
   }
+  */
+
+  onBack(): void {
+    this._router.navigate(['product']);
+  }
+
 
   ngOnDestroy() {
     this.sub.unsubscribe();
-  }
-   
-  onBack(): void {
-    this._router.navigate(['product']);
   }
 }
