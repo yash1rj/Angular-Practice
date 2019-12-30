f46d445965e68778efeeab0d8f474122342fd261
Yash Raj <yashrj130@gmail.com>
2019-12-30 16:31:11 +0530

    Create parent.component.ts

---
+++ RxJS BehaviorSubject/parent.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+import { DataService } from "../data.service";
+
+@Component({
+  selector: 'app-parent',
+  template: `
+    {{message}}
+  `,
+  styleUrls: ['./sibling.component.css']
+})
+export class ParentComponent implements OnInit {
+
+  message:string;
+
+  constructor(private data: DataService) { }
+
+  ngOnInit() {
+    this.data.currentMessage.subscribe(message => this.message = message)
+  }
+
+}
